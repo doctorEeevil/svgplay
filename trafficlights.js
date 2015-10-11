755439663f2426a45d87b191991790364d54739e
Shawn Murphy <smurp@smurp.com>
2015-10-11 16:19:29 -0600

    add get_color function and lights list

--- trafficlights.js
+++ trafficlights.js
@@ -1,5 +1,15 @@
-alert("Javascript file loaded.");
+// alert("Javascript file loaded.");
+
+var lights = {
+    redlight:    {off: 'firebrick',     on: 'orangered'},
+    yellowlight: {off: 'darkgoldenrod', on: 'yellow'},
+    greenlight:  {off: 'darkgreen',     on:'lime'}
+};
+
+var get_color = function(light_id, state) {
+    return lights[light_id][state];
+};
 var turn_light = function (light_id, state) {
     var elem = document.getElementById(light_id);
-    elem.setAttribute("fill", "lime")
+    elem.setAttribute("fill", get_color(light_id, state))
 };
