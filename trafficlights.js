da28e52527073ae08efaad39f9d821c319c38337
Samuel Aiudi-Murphy <doctorEeevil@gmail.com>
2015-10-11 17:11:30 -0600

    make everything good and happy

--- trafficlights.js
+++ trafficlights.js
@@ -1,4 +1,5 @@
 // alert("Javascript file loaded.");
+var current_color = "redlight";
 
 var lights = {
     redlight:    {off: 'firebrick',     on: 'orangered'},
@@ -9,7 +10,22 @@ var lights = {
 var get_color = function(light_id, state) {
     return lights[light_id][state];
 };
-var turn_light = function (light_id, state) {
+var turn_light = function(light_id, state) {
     var elem = document.getElementById(light_id);
     elem.setAttribute("fill", get_color(light_id, state))
+};
+var cycle_lights = function () {
+    turn_light(current_color, "off");
+    switch (current_color) {
+    case "redlight":
+	current_color = "greenlight";
+	break;
+    case "greenlight":
+	current_color = "yellowlight";
+	break;
+    case "yellowlight":
+	current_color = "redlight";
+	break;
+    }
+    turn_light(current_color, "on");
 };
